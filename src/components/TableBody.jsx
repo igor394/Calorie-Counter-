b04162ebb558f6af608e2a4b80e47a9e1fb43dba
igor394 <svidunovich-igor@rambler.ru>
2021-05-06 12:05:19 +0300

    create all task

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -3,19 +3,34 @@ import React from 'react';
 
 const TableBody = ({array}) => {
 
-    return(
-        <tbody>
-        {array.map((item, index) =>
-            <tr key={index}>
-                <td>{index+1}</td>
-                <td>{item['Food_Code']}</td>
-                <td>{item['Display_Name']}</td>
-                <td>{item['Portion_Display_Name']}</td>
-                <td>{item['Calories']}</td>
+    let out = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i > 0 && array[i].Display_Name !== array[i - 1].Display_Name) {
+            out.push(<tr key={i} style={{borderTop: '2px solid black'}}>
+                <td>{i + 1}</td>
+                <td>{array[i]['Food_Code']}</td>
+                <td>{array[i]['Display_Name']}</td>
+                <td>{array[i]['Portion_Display_Name']}</td>
+                <td>{array[i]['Calories']}</td>
+            </tr>)
+        } else {
+            out.push(<tr key={i}>
+                <td>{i + 1}</td>
+                <td>{array[i]['Food_Code']}</td>
+                <td>{array[i]['Display_Name']}</td>
+                <td>{array[i]['Portion_Display_Name']}</td>
+                <td>{array[i]['Calories']}</td>
             </tr>)
         }
+    }
+
+    return (
+        <tbody>
+            {out}
         </tbody>
     )
 };
 
 export default TableBody;
+
+
